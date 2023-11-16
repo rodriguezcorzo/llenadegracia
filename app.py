482199d0f5903b17d25878be6d7e783aed20dc03
Omar Rodriguez <orandres028@gmail.com>
2023-11-16 09:47:56 -0500

    Revisar rutas, enlaces y el block content de la pagina principal con la de eventos que no aparece

--- app.py
+++ app.py
@@ -6,5 +6,25 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/administrador')
+def administrador():
+    return render_template('administrador.html')
+
+@app.route('/formulario-evento')
+def formulario_evento():
+    return render_template('form_evento.html')
+
+@app.route('/inscripcion')
+def formulario_inscripcion():
+    return render_template('form_inscripcion.html')
+
+@app.route('/donacion')
+def formulario_donacion():
+    return render_template('form_donacion.html')
+
+@app.route('/eventos')
+def eventos():
+    return render_template('eventos.html')
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=3000)
