f0b425be4cc6b19535b65a1687cb2a8578b11ba2
rodriguezcorzo <orandres028@gmail.com>
2024-02-21 14:58:38 -0500

    Entiendo el index con la página eventos atravéz de jinja

--- routes/formulario_evento.py
+++ routes/formulario_evento.py
@@ -1,20 +1,19 @@
 from flask import render_template, Blueprint, request, redirect, url_for, session
 from config import db
 from models.evento import Evento
-from functools import wraps
 
 formulario_evento_bp = Blueprint('formulario_evento_bp', __name__)
 
-def require_admin(func):
-    @wraps(func)
-    def decorated_function(*args, **kwargs):
-        if session.get('user_role') != 'admin':
-            return redirect(url_for('app_routes.login_administrador_bp.login_administrador'))  # Redirecciona a la página de inicio de sesión si el usuario no es administrador
-        return func(*args, **kwargs)
-    return decorated_function
+# def require_admin(func):
+#     @wraps(func)
+#     def decorated_function(*args, **kwargs):
+#         if session.get('user_role') != 'admin':
+#             return redirect(url_for('app_routes.login_administrador_bp.login_administrador'))  # Redirecciona a la página de inicio de sesión si el usuario no es administrador
+#         return func(*args, **kwargs)
+#     return decorated_function
 
 @formulario_evento_bp.route('/crear_evento', methods=['GET', 'POST'])
-@require_admin
+# @require_admin
 def formulario_evento():
     if request.method == 'POST':
         titulo = request.form['nombreEvento']
@@ -23,7 +22,7 @@ def formulario_evento():
         imagenes = request.files.getlist('imagenes[]')
         costo = request.form.get('costo')
         #*el id_admin se obtiene atravez de la sesion
-        # id_admin = request.form['id_admin']
+        id_admin = request.form['id_admin']
 
         nuevo_evento = Evento(
             titulo=titulo,
@@ -32,7 +31,7 @@ def formulario_evento():
             imagen=imagenes,
             costo=costo,
             #*Se obtiene atraves de la sesions
-            #ID_admin=id_admin
+            ID_admin=id_admin
         )
 
         db.session.add(nuevo_evento)
--- routes/mostrar_eventos.py
+++ routes/mostrar_eventos.py
@@ -2,6 +2,6 @@ from flask import render_template, Blueprint
 
 mostrar_eventos_bp = Blueprint('monstrar_eventos_bp', __name__)
 
-@mostrar_eventos_bp.route('/pagina-eventos')
+@mostrar_eventos_bp.route('/Eventos')
 def mostrar_eventos():
     return  render_template('mostrar_eventos.html')
